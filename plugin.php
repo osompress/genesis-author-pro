bbdf29655eeaf4cbd63e5262e9660c44d2d296d2
Mike Hemberger <mike@thestizmedia.com>
2017-10-02 14:10:44 -0400

    remove unnecessary spacing

--- plugin.php
+++ plugin.php
@@ -65,7 +65,7 @@ define( 'GENESIS_AUTHOR_PRO_FUNCTIONS_DIR', dirname(        __FILE__ ) . '/funct
 define( 'GENESIS_AUTHOR_PRO_TEMPLATES_DIR', dirname(        __FILE__ ) . '/templates/' );
 define( 'GENESIS_AUTHOR_PRO_RESOURCES_URL', plugin_dir_url( __FILE__ ) . 'resources/'  );
 
-add_action( 'init', array( 'Genesis_Author_Pro_CPT', 'init'            ), 1 );
+add_action( 'init', array( 'Genesis_Author_Pro_CPT', 'init' ), 1 );
 
 add_action( 'genesis_init', 'genesis_author_pro_init' );
 /**
