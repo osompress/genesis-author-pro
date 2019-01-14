dcca43c01f15f9a2409e348e999cb201db10d7f3
Marcos Antonio Schratzenstaller <marksabbath@gmail.com>
2019-01-14 21:51:33 -0200

    Bumping up the version and updating the Author, adding new contributors.

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 /*
 
 Plugin Name: Genesis Author Pro
-Plugin URI:
+Plugin URI: https://wordpress.org/plugins/genesis-author-pro/
 Description: Adds default Book CPT to any Genesis HTML5 theme.
-Version: 1.0
-Author: copyblogger
-Author URI: http://www.copyblogger.com
+Version: 1.0.1
+Author: StudioPress
+Author URI: https://www.studiopress.com/
 Text Domain: genesis-author-pro
 Domain Path /languages/
 
